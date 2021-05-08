ad06b755da6b2e01d54a4216c37d0f5aa529f374
Joan Ciprià <joancipria@protonmail.com>
2021-05-08 18:26:10 +0200

    Initial commit

---
+++ static/js/components/botTyping.js
@@ -0,0 +1,21 @@
+
+/**
+ * removes the bot typing indicator from the chat screen
+ */
+function hideBotTyping() {
+    // $("#botAvatar").remove();
+    // $(".botTyping").remove();
+    document.getElementById("bot_status").innerHTML = "en línea";
+
+}
+
+/**
+ * adds the bot typing indicator from the chat screen
+ */
+function showBotTyping() {
+    // const botTyping = '<img class="botAvatar" id="botAvatar" src="./static/img/sara_avatar.png"/><div class="botTyping"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
+    // $(botTyping).appendTo(".chats");
+    // $(".botTyping").show();
+    // scrollToBottomOfResults();
+    document.getElementById("bot_status").innerHTML = "escribiendo ...";
+}
---
+++ static/js/constants.js
@@ -0,0 +1,3 @@
+const action_name = "action_dar_bienvenida";
+const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const sender_id = uuidv4();
